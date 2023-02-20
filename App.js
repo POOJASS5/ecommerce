579e689d83c72c0c058d72b94fd64ce92d7dac09
POOJA <121348592+poojaANUP@users.noreply.github.com>
2023-02-20 19:20:02 +0530

    home page

---
+++ App.js
@@ -0,0 +1,46 @@
+import React from "react";
+import Header from "./header/Header";
+import Section from "./section/Section";
+import Footer from "./footer/Footer";
+
+function App() {
+  const productsArr = [
+    {
+      title: "Colors",
+      price: 100,
+      imageUrl:
+        "https://prasadyash2411.github.io/ecom-website/img/Album%201.png",
+    },
+
+    {
+      title: "Black and white Colors",
+      price: 50,
+      imageUrl:
+        "https://prasadyash2411.github.io/ecom-website/img/Album%202.png",
+    },
+
+    {
+      title: "Yellow and Black Colors",
+      price: 70,
+      imageUrl:
+        "https://prasadyash2411.github.io/ecom-website/img/Album%203.png",
+    },
+
+    {
+      title: "Blue Color",
+      price: 100,
+      imageUrl:
+        "https://prasadyash2411.github.io/ecom-website/img/Album%204.png",
+    },
+  ];
+
+  return (
+    <React.Fragment>
+      <Header />
+      <Section productList={productsArr} />
+      <Footer />
+    </React.Fragment>
+  );
+}
+
+export default App;

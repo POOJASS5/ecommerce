f71808186f67931b4d7dc005f7987c225c33f0b3
POOJA <121348592+poojaANUP@users.noreply.github.com>
2023-02-20 19:53:10 +0530

    adding functionality to add items

--- Button.js
+++ Button.js
@@ -3,10 +3,12 @@ import classes from "./Button.module.css";
 
 const Button = (props) => {
   return (
-    <button className={`${classes.button} ${props.className}`}>
+    <button
+      className={`${classes.button} ${props.className}`}
+      onClick={props.onClick}
+    >
       {props.title}
     </button>
   );
 };
-
 export default Button;
--- Cart.js
+++ Cart.js
@@ -1,43 +1,19 @@
-import React from "react";
+import React, { useContext } from "react";
 import ReactDOM from "react-dom";
+import cartContext from "../store/cart-Context";
 
 import classes from "./Cart.module.css";
-// import Modal from '../UI/Modal';
+
 import CartItem from "./CartItem";
 
 const Cart = () => {
-  const cartElements = [
-    {
-      title: "Colors",
-      price: 100,
-      imageUrl:
-        "https://prasadyash2411.github.io/ecom-website/img/Album%201.png",
-      quantity: 2,
-    },
-
-    {
-      title: "Black and white Colors",
-      price: 50,
-      imageUrl:
-        "https://prasadyash2411.github.io/ecom-website/img/Album%202.png",
-      quantity: 3,
-    },
-
-    {
-      title: "Yellow and Black Colors",
-      price: 70,
-      imageUrl:
-        "https://prasadyash2411.github.io/ecom-website/img/Album%203.png",
-      quantity: 1,
-    },
-  ];
+  const cartCtx = useContext(cartContext);
+  const cartElements = cartCtx.item;
 
   const cartItemList = cartElements.map((item) => (
     <CartItem key={Math.random().toString()} item={item} />
   ));
-
   const root = document.getElementById("cartModal");
-
   return ReactDOM.createPortal(
     <div className={classes.overlay}>
       <span className={classes.title}>CART</span>
@@ -50,12 +26,11 @@ const Cart = () => {
       {cartItemList}
       <div className={classes.total}>
         <span>Total</span>
-        <div>₹25.99</div>
+        <div>₹{cartCtx.totalAmount.toFixed(2)}</div>
       </div>
       <button className={classes.button}>PURCHASE</button>
     </div>,
     root
   );
 };
-
 export default Cart;

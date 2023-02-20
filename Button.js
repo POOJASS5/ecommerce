8958273f16ac0004dfcde0139b2dba09209c635c
POOJA <121348592+poojaANUP@users.noreply.github.com>
2023-02-20 19:42:31 +0530

    adding cart button

--- Button.js
+++ Button.js
@@ -1,9 +1,12 @@
 import React from "react";
-
 import classes from "./Button.module.css";
 
 const Button = (props) => {
-  return <button className={classes.button}>{props.title}</button>;
+  return (
+    <button className={`${classes.button} ${props.className}`}>
+      {props.title}
+    </button>
+  );
 };
 
 export default Button;
--- MusicItems.js
+++ MusicItems.js
@@ -1,17 +1,14 @@
 import React from "react";
-
 import classes from "./MusicItems.module.css";
 import Button from "../UI/Button";
-
 const MusicItems = (props) => {
   return (
     <div className={classes.div}>
       <h2>{props.item.title}</h2>
       <img src={props.item.imageUrl} alt="Music Album" />
-      <span>${props.item.price}</span>
+      <span>₹{props.item.price}</span>
       <Button title="ADD TO CART" />
     </div>
   );
 };
-
 export default MusicItems;
